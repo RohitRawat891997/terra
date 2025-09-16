d4a35ed1d9bf560b48d28d2abcd1945b923f29a2
rohit <rohitrawat891997@gmail.com>
2025-09-16 23:20:55 +0530

    auto VPC creation

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -8,12 +8,7 @@ terraform {
 }
 
 locals {
-  AWS_REGION_DC = "us-east-1"
   AWS_REGION_DR = "us-east-2"
-}
-
-provider "aws" {
-  region = local.AWS_REGION_DC
 }
 
 provider "aws" {
@@ -21,12 +16,7 @@ provider "aws" {
   alias = "dr"
 }
 
-resource "aws_vpc" "main_dc" {
-    cidr_block = "192.168.10.0/24"
-}
-
 resource "aws_vpc" "main_dr" {
     cidr_block = "172.32.0.0/16"
     provider = aws.dr
 }
-
